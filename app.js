01f32b2e4dde7ca9d6ce962efc8b61756280df53
minhdangv <dangvuongminh25@gmail.com>
2022-09-21 12:17:15 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."});
 });
 
-app.use("/api/contacts", contactsRouter);
+//xử lí phản hồi lỗi 404
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+    });
 
+//middle-ware xử lí lỗi tập trung
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+    });
+});
 module.exports = app;
