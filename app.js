0da5b424f1914906e8a11697be4af5edea538f78
minhdangv <dangvuongminh25@gmail.com>
2022-09-21 11:26:05 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const contactsRouter = require("./app/routes/contact.route");
 const app = express();
 
 app.use(cors());
@@ -9,5 +9,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."});
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -6,4 +6,14 @@ const router = express.Router();
 router.route("/")
     .get(contacts.findAll)
     .post(contacts.create)
-    .delete
+    .delete(contacts.deleteAll);
+
+router.route("/favorite")
+    .get(contacts.findAllFavorite);
+
+router.route("/:id")
+    .get(contacts.findOne)
+    .put(contacts.update)
+    .delete(contacts.delete);
+
+module.exports = router;
